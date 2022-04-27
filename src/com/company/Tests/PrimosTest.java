be6253a6e154fd08a481dac5d7db6a01ba2e20e9
Cristian <alunac6050@ieselcaminas.org>
2022-04-27 20:08:40 +0200

    2

--- src/com/company/Tests/PrimosTest.java
+++ src/com/company/Tests/PrimosTest.java
@@ -7,13 +7,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PrimosTest {
 
+    int[] vacio = new int[0];
+    int[] uno = {2};
+    int[] cien = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+
     @Test
-    void generarPrimos() {
-        int numeroMaximo = 40;
-        Primos primosClase = new Primos(numeroMaximo);
-        int[] primos = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+    void generarPrimos_100() {
+        Primos primosClase = new Primos(100);
         int[] primosMetodo = primosClase.generarPrimos();
 
-        assertArrayEquals(primos, primosMetodo);
+        assertArrayEquals(cien, primosMetodo);
+    }
+
+    @Test
+    void generarPrimos_vacio_1() {
+        Primos primosClase = new Primos(1);
+        int[] primosMetodo = primosClase.generarPrimos();
+
+        assertArrayEquals(vacio, primosMetodo);
+    }
+    @Test
+    void generarPrimos_vacio_0() {
+        Primos primosClase = new Primos(0);
+        int[] primosMetodo = primosClase.generarPrimos();
+
+        assertArrayEquals(vacio, primosMetodo);
+    }
+    @Test
+    void generarPrimos_1() {
+        Primos primosClase = new Primos(2);
+        int[] primosMetodo = primosClase.generarPrimos();
+
+        assertArrayEquals(uno, primosMetodo);
     }
 }
