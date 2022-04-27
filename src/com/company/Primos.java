24fde30c3ec30ed2d300ee9e967f37bc85fe7506
Cristian <alunac6050@ieselcaminas.org>
2022-04-27 19:44:21 +0200

    2

--- src/com/company/Primos.java
+++ src/com/company/Primos.java
@@ -1,13 +1,25 @@
 package com.company;
 
+/**
+ * Esta clase sirve para generar todods los numeros primos
+ * hasta un numero maximo que tu elejiges
+ * @Autor Cristian Nacher Collado
+ * @Version Documentada
+ */
+
 public class Primos {
-    // Generar números primos de 1 a max
+
     private int max;
 
     public Primos(int max) {
         this.max = max;
     }
 
+    /**
+     * El metodo rellena un array booleana
+     * @param dim nos dice el tamaño del array
+     * @return nos devulve la array booleana llena
+     */
     public boolean[] iniciaArray(int dim) {
         boolean[] esPrimo = new boolean[dim];
         for (int i = 0; i < dim; i++) {
@@ -16,6 +28,14 @@ public class Primos {
         return esPrimo;
     }
 
+    /**
+     * El metodo nos comprueba los numeros primos hasta el numero que le pasemos
+     * y nos devulve una array booleana con las posiciones marcadas con un true si
+     * son primos.
+     * @param dim nos marcha el numero maximo hasta el que tenemos que comprovar los primos
+     * @param esPrimo es una array booleana para marcar los numeros primos
+     * @return nos devuelve la array booleana con los primos marcados
+     */
     public boolean[] comprovarPrimos(int dim, boolean[] esPrimo) {
         for (int i = 2; i < Math.sqrt(dim) + 1; i++) {
             if (esPrimo[i]) {
@@ -28,6 +48,12 @@ public class Primos {
         return esPrimo;
     }
 
+    /**
+     * El metodo nos cuenta el numero de primos que tenemos
+     * @param dim nos sirve para saber quando parar el bucle en el metodo
+     * @param esPrimo es la array donde tenemos los primos marcados
+     * @return nos devuelve un int con el numero de primos
+     */
     public int cuentaPrimos(int dim, boolean[] esPrimo) {
         int cuenta = 0;
         for (int i = 0; i < dim; i++) {
@@ -38,6 +64,15 @@ public class Primos {
         return cuenta;
     }
 
+    /**
+     * El metodo nos crea una array de ints con los primos que hay hasta el numero
+     * maximo que le pasamos
+     * @param dim nos marca el numero maximo para saber cuando parar el bucle
+     * @param numPrimos nos sirver para saber el tamaño del array que rellenaremos de
+     *                  primos (lo hemos sacado en el metodo anterior)
+     * @param esPrimo es el array de booleanos que nos marca los numero primos
+     * @return nos devuelve una array de ints con los numeros primos
+     */
     public int[] rellenaVectorDePrimos(int dim, int numPrimos, boolean[] esPrimo){
         int[] primos= new int[numPrimos];
         for (int i = 0, j = 0; i < dim; i++) {
@@ -47,6 +82,12 @@ public class Primos {
         }
         return primos;
     }
+
+    /**
+     * Este metodo basicamente junta todos los metodos anteriores para que dado el numero
+     * maximo nos devuelva una array con los numeros primos
+     * @return nos devuelve una array con los numeros primos.
+     */
 
     public int[] generarPrimos() {
         if (max >= 2) {
