5de975e14883bee49fe18012f0e0749a2099c0ab
Cristian <alunac6050@ieselcaminas.org>
2022-04-27 20:35:34 +0200

    6

---
+++ src/com/company/Tests/PrimosTest.java
@@ -0,0 +1,12 @@
+package com.company.Tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimosTest {
+
+    @Test
+    void generarPrimos() {
+    }
+}
